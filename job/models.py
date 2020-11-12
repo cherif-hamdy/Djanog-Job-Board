561dc43b671db27a1cbc27dec28b36aed1be61d0
Sherif Hamdy <Sherif.Hamdy@smartsense.com>
2020-11-12 10:20:29 +0200

    adding views logic

--- job/models.py
+++ job/models.py
@@ -5,6 +5,15 @@ JOB_Type = (
     ('Full Time','Full Time'),
     ('Part Time','Part Time'),
 )
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=25)
+    
+    def __str__(self):
+        return self.name
+
+
 class Job(models.Model):
     title = models.CharField(max_length=100)
     job_type = models.CharField(max_length=20,choices=JOB_Type)
@@ -13,6 +22,7 @@ class Job(models.Model):
     vacancy = models.IntegerField(default=1)
     salary = models.IntegerField(default=0)
     experience = models.IntegerField(default=1)
+    category = models.ForeignKey("Category" , on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
--- job/views.py
+++ job/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Job , Category
 
 # Create your views here.
+def job_list(request):
+    jobs =  Job.objects.all()
+    return HttpResponse(jobs)
+
+
+def job_detail(request , id):
+    return HttpResponse("hello world") 
